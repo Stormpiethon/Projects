78f1dabdde0cc95d1abe30db2bfdad82bdee323b
Alexander Brittain <105312701+Stormpiethon@users.noreply.github.com>
2025-10-16 14:03:10 -0700

    Clarify comments in BaseLayer class
    Updated class Layer comments for clarity.

--- Neural Network/BaseLayer.py
+++ Neural Network/BaseLayer.py
@@ -1,9 +1,10 @@
-# Base class that all the different layers of the Neural Network can inherit from
+# Base class that all the different layers of the Neural Network can inherit from, basically abstract
 class Layer:
     def __init__(self):
         self.input = None
         self.output = None
 
+    # These functions are overridden in the other layers
     def forward(self, input):
         # TODO: return output
         pass
@@ -11,4 +12,5 @@ class Layer:
     def backward(self, output_gradient, learning_rate):
         # TODO: update parameters and return input_gradient
         pass
+
 
